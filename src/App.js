29e7b0f75d6ac3ba57ac36e887aed2e8c081067e
Edwin Caro <edwincaro10@hotmail.com>
2020-12-31 01:49:01 +0000

    second commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import http from "http";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1> Hello World From Reactjs </h1>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+// http.createServer("options", "app").listen(8080, () => {})
+
+export default App;
